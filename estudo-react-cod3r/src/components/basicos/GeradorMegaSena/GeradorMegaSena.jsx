8bf61db91f0f4daba26120110373f8c11c718e2f
psnroberto <robertop.souzaneto@gmail.com>
2023-07-31 14:07:15 -0300

    CMT4
    Desafio Gerador de número Mega Sena.

--- estudo-react-cod3r/src/components/basicos/GeradorMegaSena/GeradorMegaSena.jsx
+++ estudo-react-cod3r/src/components/basicos/GeradorMegaSena/GeradorMegaSena.jsx
@@ -1,5 +1,6 @@
 
 import { useState } from 'react'
+import './GeradorMegaSena.css'
 
 function GeradorMegaSena(props) {
 
@@ -10,53 +11,59 @@ function GeradorMegaSena(props) {
 
     let numerosArray = []
 
-
     function gerarNumeros() {
-
 
         for (let i = 0; i <= quantidade - 1; i++) {
             let numero = Math.floor(Math.random() * (max - min + 1)) + min
+
+            while (numerosArray.includes(numero)) {
+                numero = Math.floor(Math.random() * (max - min + 1)) + min
+            }
+
             numerosArray.push(numero)
         }
-        
+
         resultado = numerosArray
-        setResultado(numerosArray)
-
-        window.alert(resultado)
-
+        setResultado(numerosArray.join(' - '))
 
     }
 
     return (
-        <div>
+        <div className='megasena'>
 
             <h2>{resultado}</h2>
 
-            <label htmlFor="quantidade">Informe quantos números serão gerados:</label>
-            <input
-                type="number"
-                id='quantidade'
-                name="quantidade"
-                onChange={(event) => { setQuantidade(Number(event.target.value)) }}
-            />
+            <div className='inputs'>
+                <label htmlFor="quantidade">Informe quantos números serão gerados:</label>
+                <input
+                    type="number"
+                    id='quantidade'
+                    name="quantidade"
+                    onChange={(event) => { setQuantidade(Number(event.target.value)) }}
+                />
+            </div>
 
-            <label htmlFor="min">Informe o valor mínimo:</label>
-            <input
-                type="number"
-                id='min'
-                name="min"
-                onChange={(event) => { setMin(event.target.value) }}
-            />
+            <div className='inputs'>
+                <label htmlFor="min">Informe o valor mínimo:</label>
+                <input
+                    type="number"
+                    id='min'
+                    name="min"
+                    onChange={(event) => { setMin(Number(event.target.value)) }}
+                />
+            </div>
 
-            <label htmlFor="max">Informe o valor máximo:</label>
-            <input
-                type="number"
-                id='max'
-                name="max"
-                onChange={(event) => { setMax(event.target.value) }}
-            />
+            <div className='inputs'>
+                <label htmlFor="max">Informe o valor máximo:</label>
+                <input
+                    type="number"
+                    id='max'
+                    name="max"
+                    onChange={(event) => { setMax(Number(event.target.value)) }}
+                />
+            </div>
 
-            <button onClick={gerarNumeros}>Gerar números!</button>
+            <button className='button' onClick={gerarNumeros}>Gerar números!</button>
 
         </div>
     )
